1f5c4050a19067b6f358f34161d1477077b62a13
NirmalaDS <71771952+NirmalaDS@users.noreply.github.com>
2020-10-02 11:49:58 -0400

    Update GHappyTest.java

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,42 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
 public class GHappyTest {
+
+    private GHappy gh;
+    @BeforeEach
+    public void initialize() {this.gh = new GHappy();}
+
+    @Test
+    public void OneHappyg() {
+        boolean result = gh.gHappy("ddggvvvaa");
+        Assertions.assertTrue(result);
+    }
+    @Test
+    public void ManyHappyg() {
+        boolean result = gh.gHappy("vvvggaaggg");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void NotHappyg(){
+        boolean result = gh.gHappy("hijigui");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void FirstUnhappyg(){
+        boolean result = gh.gHappy("ghijiui");
+        Assertions.assertFalse(result);
+
+    }
+    @Test
+    public void LastUnhappyg() {
+        boolean result = gh.gHappy("hijiuig");
+        Assertions.assertFalse(result);
+    }
 }
